6678e419d1fceb39a70f0f2183d071781a67a750
avrezzon <anthonyrezz@gmail.com>
2019-05-01 14:43:15 -0600

    Final wrap up

--- AirYourBnb/run-job.sh
+++ AirYourBnb/run-job.sh
@@ -7,10 +7,10 @@
 #$5 will be the number relating to who's HDFS to connect to. 1=anthony 2=daniel 3=bruce
 
 if [ "$#" -ne 5 ]; then
-    echo "Usage: ./run-job.sh [namenode] [portnumber] [class of driver] [jar name in target dir] [java args]"
+    echo "Usage: ./run-job.sh [namenode] [portnumber] [class of driver] [jar name in target dir] [args]"
 	exit
 fi
-echo $#
+sbt package
 echo $SPARK_HOME/bin/spark-submit --master spark://$1:$2 --deploy-mode cluster --class $3 --supervise target/scala-2.11/$4_2.11-1.0.jar $5
 $SPARK_HOME/bin/spark-submit --master spark://$1:$2 --deploy-mode cluster --class $3 --supervise target/scala-2.11/$4_2.11-1.0.jar $5
-$HADOOP_HOME/bin/hadoop dfs -rmr /debug/
+
